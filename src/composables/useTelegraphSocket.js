994b5546fc53e90a7132b8f3933ff4524221e1ab
Efe <efebideci@gmail.com>
2026-08-15 16:34:37 +0300

    fix3: more UI feedback

--- src/composables/useTelegraphSocket.js
+++ src/composables/useTelegraphSocket.js
@@ -161,8 +161,8 @@ export function useTelegraphSocket(wheelEngine) {
         }
 
         if (data.type === 'PEER_STATUS') {
-          peerCount.value = data.peerCount;
           const previousCount = peerCount.value;
+          peerCount.value = data.peerCount;
           if (data.message) showNotification(data.message);
 
           if (data.peerCount > previousCount) {
