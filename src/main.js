0ebf447cb0974d6112f1d9452dd121fc85b78c07
Efe <efebideci@gmail.com>
2026-08-15 18:15:34 +0300

    analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { registerSW } from 'virtual:pwa-register'
+import { Analytics } from "@vercel/analytics/next"
 
 registerSW({ immediate: true })
 
